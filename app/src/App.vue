1d9fec85fe95cc177af78a29d22999a06f1bb651
swanie21 <kswanie21@gmail.com>
2016-12-05 17:04:25 -0700

    render home page component

--- app/src/App.vue
+++ app/src/App.vue
@@ -11,12 +11,6 @@
 
   body {
     align-items: center;
-    background:
-      radial-gradient(
-        ellipse at center,
-        rgba(255, 255, 255, 1) 0%,
-        rgba(229, 229, 229, .85) 100%
-      );
     background-position: center;
     display: flex;
     font-family: Lato, Helvetica, sans-serif;
@@ -27,15 +21,15 @@
 
 <template>
   <div>
-    <landing-page></landing-page>
+    <home-page></home-page>
   </div>
 </template>
 
 <script>
-  import LandingPage from './components/LandingPageView'
+  import HomePage from './components/HomePage'
   export default {
     components: {
-      LandingPage
+      HomePage
     }
   }
 </script>
--- app/src/components/LandingPageView.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div>
-    <h1>hey</h1>
-  </div>
-</template>
