277e7cbd52b6023bfc27395c9c7507b831e53ec7
swanie21 <kswanie21@gmail.com>
2016-12-10 22:49:28 -0700

    add karma tests

--- test/e2e/specs/Launch.spec.js
+++ test/e2e/specs/Launch.spec.js
@@ -1,14 +1,29 @@
-import utils from '../utils'
-import { name } from '../../../config'
+'use strict';
+import utils from '../utils';
+import { name } from '../../../config';
+import Vue from 'Vue';
+// import HomePage from '../../app/src/components/HomePage';
 
 describe('Launch', function () {
-  beforeEach(utils.beforeEach)
-  afterEach(utils.afterEach)
+  beforeEach(utils.beforeEach);
+  afterEach(utils.afterEach);
 
   it('shows the proper application title', function () {
     return this.app.client.getTitle()
     .then(title => {
-      expect(title).to.equal('Change of Scenery')
-    })
-  })
-})
+      expect(title).to.equal('Change of Scenery');
+    });
+  });
+});
+
+describe('Launch', function () {
+  it('should render correct message', function () {
+    var vm = new Vue({
+      template: '<div><test></test></div>',
+      components: {
+        'test': HomePage
+      }
+    }).$mount()
+    expect(vm.$el.querySelector('submit-search-button').textContent).toBe('Get Image')
+  });
+});
