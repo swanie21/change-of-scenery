2b4a5f2b68952fcd8b1e2f5e9322c5fbf34b68e2
swanie21 <kswanie21@gmail.com>
2016-12-10 22:53:52 -0700

    removed e2e test

--- test/e2e/specs/Launch.spec.js
+++ test/e2e/specs/Launch.spec.js
@@ -2,7 +2,6 @@
 import utils from '../utils';
 import { name } from '../../../config';
 import Vue from 'Vue';
-// import HomePage from '../../app/src/components/HomePage';
 
 describe('Launch', function () {
   beforeEach(utils.beforeEach);
@@ -13,17 +12,5 @@ describe('Launch', function () {
     .then(title => {
       expect(title).to.equal('Change of Scenery');
     });
-  });
-});
-
-describe('Launch', function () {
-  it('should render correct message', function () {
-    var vm = new Vue({
-      template: '<div><test></test></div>',
-      components: {
-        'test': HomePage
-      }
-    }).$mount()
-    expect(vm.$el.querySelector('submit-search-button').textContent).toBe('Get Image')
   });
 });
